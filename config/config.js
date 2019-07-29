e8bb1fcfdb9beae90048c12315f9717c004245b4
ScottMCarr <biz.scott.carr@gmail.com>
2019-07-29 19:11:49 +0800

    Add development specific config.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,16 @@
-exports = {
-    port: 80,
+let port;
+let mongoUri;
+if (process.env.dev) {
+    port = 8080;
+    mongoUri = 'mongodb://localhost:27017/volet';
+} else {
+    port = 80;
+    mongoUri = 'mongodb://localhost:27017/volet';
+}
+module.exports = {
+    port: port,
     mongoose: {
-        uri: 'mongodb://mongo:27017/volet',
+        uri: mongoUri,
         options: { useNewUrlParser: true }
     },
     secret: 'blueninja'
