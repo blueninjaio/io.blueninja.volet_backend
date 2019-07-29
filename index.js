aa0892548bb52fd98fbc85518cc9d506d1eacb02
ScottMCarr <biz.scott.carr@gmail.com>
2019-07-29 19:10:53 +0800

    Add routes to index.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require("./config/config");
 const app = express();
 const path = require('path');
 
@@ -15,9 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-});
+app.use(`/api`, routes);
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
+    console.log(`Server running on port: ${port}`);
 });
