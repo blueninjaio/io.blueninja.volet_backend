921bff17bfa052d3821da301d97ad438f9808d0b
Ken Yap Wei Chun <wei.chun@pickupp.io>
2019-09-20 14:22:14 +0800

    Modifying `factory.js`
    - Changing to `chance.email` for admin.email
    - Adding merchant into the factory
    - Adding Merchant model tests

--- tests/factory.js
+++ tests/factory.js
@@ -7,6 +7,18 @@ chai.use(factories)
 
 // declaring a mock user
 chai.factory('admin', {
-  email: `${chance.word({ length: 3 })}.${chance.word({ length: 5 })}@${chance.domain()}`,
+  email: chance.email(),
   password: chance.word({ length: 10 }),
+})
+
+chai.factory('merchant', {
+  contact: chance.word({ length: 10 }),
+  f_name: chance.first(),
+  l_name: chance.last(),
+  email: chance.email(),
+  password: chance.word({ length: 10 }),
+  photo_url: chance.url({path: 'images'}),
+  address: chance.address(),
+  push_token: chance.word({ length: 20 }),
+  credits: 0,
 })
