2190bd7fc41a0dfe4df5096b0f1328f15a2194ad
Ken Yap Wei Chun <wei.chun@pickupp.io>
2019-09-25 10:46:51 +0800

    Removing timekeeper that causing `FakeDate` issue

--- tests/app.js
+++ tests/app.js
@@ -6,7 +6,6 @@ process.env.NODE_ENV = 'test'
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const chaiThings = require('chai-things')
-const timekeeper = require('timekeeper')
 const mongoose = require('mongoose')
 
 chai.use(chaiThings)
@@ -22,13 +21,6 @@ const clearDB = (callback) => {
   return
 }
 
-const now = new Date()
-now.setHours(10)
-now.setMinutes(0)
-now.setSeconds(0)
-now.setMilliseconds(0)
-timekeeper.freeze(now)
-
 require('./factory')
 
 console.log('Tests starting...')
@@ -38,7 +30,6 @@ before(() => {
 })
 
 after((done) => {
-  timekeeper.reset()
   clearDB()
   console.log('Tests done!')
   done()
